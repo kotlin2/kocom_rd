ba183fb3e0ee7490adf2f38523088afd7b2453e1
kotlin2 <90229619+kotlin2@users.noreply.github.com>
2024-12-27 20:41:23 +0900

    Update kocom.py

--- kocom.py
+++ kocom.py
@@ -282,10 +282,8 @@ def parse(hex_data):
     return ret
 
 
-def thermo_parse(value):
-   # ret = { 'heat_mode': 'heat' if value[:2]=='11' else 'off',
-     ret = { 'heat_mode': 'heat' if value[:4] == '1100' else 'fan_only' if value[:4] == '1101' else 'off',
-   #         'away': 'true' if value[2:4]=='01' else 'false',
+def thermo_parse(value): 
+    ret = { 'heat_mode': 'heat' if value[:4] == '1100' else 'fan_only' if value[:4] == '1101' else 'off',
             'set_temp': int(value[4:6], 16) if value[:2]=='11' else int(config.get('User', 'init_temp')),
             'cur_temp': int(value[8:10], 16)}
     return ret
